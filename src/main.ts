c311ccdee67672ae24cc8ca98d706d34e48c99fb
Ole Tjernstad <ott556@gmail.com>
2023-07-05 15:41:50 +0200

    prevent brower lock

--- src/main.ts
+++ src/main.ts
@@ -33,8 +33,12 @@ export class TuEditor {
         activeElement = activeElement?.parentElement;
       }
       // find next to editor div parent
+      let levels = 0;
       while (activeElement?.parentElement?.id !== "tu-editor") {
+        // prevent looping if tu editor is not found
+        if (levels > 10) break;
         activeElement = activeElement?.parentElement;
+        levels++;
       }
 
       if (activeElement instanceof HTMLElement) {
